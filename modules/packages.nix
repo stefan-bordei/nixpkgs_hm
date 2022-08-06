a405735120471bd854e016c13d653f3bef65aa25
stefan-bordei <stefan.bordei13@gmail.com>
2022-08-06 18:28:13 +0100

    Fix alacritty transparency

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,27 +1,5 @@
 { config, pkgs, libs, ... }:
 let
-  # currently, there is some friction between sway and gtk:
-  # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
-  # the suggested way to set gtk settings is with gsettings
-  # for gsettings to work, we need to tell it where the schemas are
-  # using the XDG_DATA_DIR environment variable
-  # run at the end of sway config
-  #configure-gtk = pkgs.writeTextFile {
-  #  name = "configure-gtk";
-  #  destination = "/bin/configure-gtk";
-  #  executable = true;
-  #  text = let
-  #    schema = pkgs.gsettings-desktop-schemas;
-  #    datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-  #  in ''
-  #    export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-  #    gnome_schema=org.gnome.desktop.interface
-  #    gsettings set $gnome_schema gtk-theme 'Materia-dark-compact'
-  #    gsettings set $gnome_schema icon-theme 'Gnome'
-  #    gsettings set $gnome_schema cursor-theme 'Gnome'
-  #    gsettings set $gnome_schema font-name 'Source Sans 10'
-   # '';
-  #};
 in {
   home.packages = with pkgs; [
     # utilities
