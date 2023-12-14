cad052ba21326907aa0f0c5b84c0e915894a7e74
stefan-bordei <stefan.bordei13@gmail.com>
2023-12-14 16:50:39 +0000

    Update waybar theme.

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -5,5 +5,5 @@ in
 {
     xdg.configFile."waybar/config".source = config.lib.file.mkOutOfStoreSymlink "${nixConfigDir}/configs/waybar/config";
     xdg.configFile."waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${nixConfigDir}/configs/waybar/style.css";
-    #xdg.configFile."waybar/weather.sh".source = config.lib.file.mkOutOfStoreSymlink "${nixConfigDir}/configs/polybar/weather.sh";
+    xdg.configFile."waybar/gruvbox.css".source = config.lib.file.mkOutOfStoreSymlink "${nixConfigDir}/configs/waybar/gruvbox.css";
 }
